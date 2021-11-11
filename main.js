f1822b9c29fdad0266e5daccfa013d3cefd80e5d
SamCares <44313759+SamCares@users.noreply.github.com>
2021-11-11 14:44:38 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,67 @@
+const form = document.querySelector('.form');
+const firstName = document.querySelector('first-name');
+const lastName = document.querySelector('last-name');
+const email = document.querySelector('email');
+const password = document.querySelector('password');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs() {
+    const firstNameValue = firstName.value.trim(); //removes whitespace
+    const lastNameValue = lastName.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (firstNameValue === '') {
+        //add error class
+        //show error icon
+        setError(firstName, 'First Name cannot be empty');
+    }
+
+    if (lastNameValue === '') {
+        //add error class
+        //show error icon
+        setError(lastName, 'Last Name cannot be empty');
+    }
+
+    if (emailValue === '') {
+        //add error class
+        //show error icon
+        setError(email, 'Email cannot be empty');
+    } else (!emailValid(emailValue))
+    setError(email, 'Looks like this is not an email')
+
+    if (passwordValue === '') {
+        //add error class
+        //show error icon
+        setError(password, 'Password cannot be empty');
+    }
+}
+
+
+
+function setError(input, message) {
+    const formControl = input.parentElement; //.form-control
+    const small = formControl.querySelector('small');
+
+    //add error message 
+    small.innerText = message;
+
+    //add error class 
+    formControl.className = 'form-control error';
+}
+
+// function setSuccess(input) {
+//     const formControl = input.parentElement;
+
+//     //add success class
+//     formControl.className = 'form-control success';
+// }
+
+function emailValid(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+} 
