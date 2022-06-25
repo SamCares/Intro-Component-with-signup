9f83f9a69bbace6331fd3f3328159a2411089987
SamCares <44313759+SamCares@users.noreply.github.com>
2022-06-25 22:14:55 +0100

    update

--- main.js
+++ main.js
@@ -1,53 +1,67 @@
 const form = document.getElementById('form');
-const firstName = document.getElementById('first_name');
-const lastName = document.getElementById('last_name');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
+// const firstName = document.getElementById('first_name');
+// const lastName = document.getElementById('last_name');
+// const email = document.getElementById('email');
+// const password = document.getElementById('password');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    function checkInputs() {
+        const firstNameErrorIcon = document.querySelector('#first_name_error_icon');
+        const firstNameErrorMessage = document.querySelector('#first_name_error_message');
+        if (form['first_name'].value.trim() === '') {
+            firstNameErrorIcon.style.visibility = 'visible';
+            firstNameErrorMessage.style.visibility = 'visible';
+            inputErrorBorder();
+        }
+
+        const lastNameErrorIcon = document.querySelector('#last_name_error_icon');
+        const lastNameErrorMessage = document.querySelector('#last_name_error_message');
+        if (form['last_name'].value.trim() === '') {
+            lastNameErrorIcon.style.visibility = 'visible';
+            lastNameErrorMessage.style.visibility = 'visible';
+            inputErrorBorder();
+        }
+
+        const emailErrorIcon = document.querySelector('#email_error_icon');
+        const emailErrorMessage = document.querySelector('#email_error_message');
+        const textSwap = document.querySelector('#text_swap');
+        if (form['email'].value.trim() === '') {
+            emailErrorIcon.style.visibility = 'visible';
+            emailErrorMessage.style.visibility = 'visible';
+            inputErrorBorder();
+        } else if (!emailValid(form['email'].value)) {
+            emailErrorIcon.style.visibility = 'visible';
+            emailErrorMessage.style.visibility = 'visible';
+            textSwap.innerText = 'Looks like this is not an email';
+            inputErrorBorder();
+        }
+
+        const passwordErrorIcon = document.querySelector('#password_error_icon');
+        const passwordErrorMessage = document.querySelector('#password_error_message');
+        if (form['password'].value.trim() === '') {
+            passwordErrorIcon.style.visibility = 'visible';
+            passwordErrorMessage.style.visibility = 'visible';
+            inputErrorBorder();
+        }
+    }
     checkInputs();
 });
+// form.submit(form);
 
-function checkInputs() {
-    const firstNameErrorIcon = document.querySelector('#first_name_error_icon');
-    const firstNameErrorMessage = document.querySelector('#first_name_error_message');
-    if (firstName.value.trim() === '') {
-        firstNameErrorIcon.style.visibility = 'visible';
-        firstNameErrorMessage.style.visibility = 'visible';
-        // input.style.border = '2px solid hsl(0, 100%, 74%)';
-    }
-
-    const lastNameErrorIcon = document.querySelector('#last_name_error_icon');
-    const lastNameErrorMessage = document.querySelector('#last_name_error_message');
-    if (lastName.value.trim() === '') {
-        lastNameErrorIcon.style.visibility = 'visible';
-        lastNameErrorMessage.style.visibility = 'visible';
-    }
-
-    const emailErrorIcon = document.querySelector('#email_error_icon');
-    const emailErrorMessage = document.querySelector('#email_error_message');
-    if (email.value.trim() === '') {
-        emailErrorIcon.style.visibility = 'visible';
-        emailErrorMessage.style.visibility = 'visible';
-    } else if (!emailValid(email.value)) {
-        emailErrorMessage.textContent = `Looks like this is not an email`;
-    }
-
-    const passwordErrorIcon = document.querySelector('#password_error_icon');
-    const passwordErrorMessage = document.querySelector('#password_error_message');
-    if (password.value.trim() === '') {
-        passwordErrorIcon.style.visibility = 'visible';
-        passwordErrorMessage.style.visibility = 'visible';
-    }
-    return;
-}
 
 function emailValid(email) {
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
 
+function inputErrorBorder() {
+    const inputErrorBorder = document.querySelectorAll('input');
+    inputErrorBorder.forEach(inputErrorBorder => {
+        inputErrorBorder.style.border = '2px solid hsl(0, 100%, 74%)';
 
+    });
+}
 
 
 
