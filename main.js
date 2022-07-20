34e63c19906fd7a1e95a9cc0480dcaddbb886812
SamCares <44313759+SamCares@users.noreply.github.com>
2022-07-20 22:51:01 +0100

    final updte

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
 const form = document.getElementById('form');
-// const firstName = document.getElementById('first_name');
-// const lastName = document.getElementById('last_name');
-// const email = document.getElementById('email');
-// const password = document.getElementById('password');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -29,7 +25,7 @@ form.addEventListener('submit', (e) => {
     } else if (!emailValid(form['email'].value)) {
         let text_swap = document.getElementById('text_swap');
         form['email'].classList.add('error_icon');
-        // text_swap.innerText = 'Looks like this is not an email';
+        text_swap.innerText = 'Looks like this is not an email';
         error = true;
     } else {
         form['email'].classList.remove('error_icon');
@@ -51,95 +47,3 @@ form.addEventListener('submit', (e) => {
 function emailValid(email) {
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
-
-// function inputErrorBorder() {
-//     const inputErrorBorder = document.querySelectorAll('input');
-//     inputErrorBorder.forEach(inputErrorBorder => {
-//         inputErrorBorder.style.border = '2px solid hsl(0, 100%, 74%)';
-//     });
-// }
-
-
-
-// function checkInputs() {
-//     if (firstName.value === '') {
-//         errorIcon.forEach(errorIcon => {
-//             errorIcon.style.visibility = 'visible'
-//         });
-//         errorMessage.forEach(errorMessage => {
-//             errorMessage.style.visibility = 'visible'
-//         });
-//         input.forEach(input => {
-//             input.style. = '2px solid hsl(0, 100%, 74%)'
-//         });
-//     }
-// }
-
-
-// const form = document.querySelector('.form');
-// const firstName = document.querySelector('first-name');
-// const lastName = document.querySelector('last-name');
-// const email = document.querySelector('email');
-// const password = document.querySelector('password');
-
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-
-//     checkInputs();
-// });
-
-// function checkInputs() {
-//     const firstNameValue = firstName.value.trim(); //removes whitespace
-//     const lastNameValue = lastName.value.trim();
-//     const emailValue = email.value.trim();
-//     const passwordValue = password.value.trim();
-
-//     if (firstNameValue === '') {
-//         //add error class
-//         //show error icon
-//         setError(firstName, 'First Name cannot be empty');
-//     }
-
-//     if (lastNameValue === '') {
-//         //add error class
-//         //show error icon
-//         setError(lastName, 'Last Name cannot be empty');
-//     }
-
-//     if (emailValue === '') {
-//         //add error class
-//         //show error icon
-//         setError(email, 'Email cannot be empty');
-//     } else (!emailValid(emailValue))
-//     setError(email, 'Looks like this is not an email')
-
-//     if (passwordValue === '') {
-//         //add error class
-//         //show error icon
-//         setError(password, 'Password cannot be empty');
-//     }
-// }
-
-
-
-// function setError(input, message) {
-//     const formControl = input.parentElement; //.form-control
-//     const small = formControl.querySelector('small');
-
-//     //add error message
-//     small.innerText = message;
-
-//     //add error class
-//     formControl.className = 'form-control error';
-// }
-
-// // function setSuccess(input) {
-// //     const formControl = input.parentElement;
-
-// //     //add success class
-// //     formControl.className = 'form-control success';
-// // }
-
-// function emailValid(email) {
-//     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-// } 
